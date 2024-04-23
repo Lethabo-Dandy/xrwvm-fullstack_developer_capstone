3a4140c111ab3d529983f8f167867b553ea89935
YousufHashmi786111726 <156332624+YousufHashmi786111726@users.noreply.github.com>
2024-04-23 18:12:54 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://yousufhashmi-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-                 'https://yousufhashmi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://yousufhashmi-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-                        'https://yousufhashmi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://yousufhashmi-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', # noqa
+                 'https://yousufhashmi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'] # noqa
+CSRF_TRUSTED_ORIGINS = ['https://yousufhashmi-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', # noqa
+                        'https://yousufhashmi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'] # noqa
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
