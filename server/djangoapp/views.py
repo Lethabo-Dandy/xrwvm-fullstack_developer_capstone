b74688199778eb8c5d73518f1f44726c56442d26
YousufHashmi786111726 <156332624+YousufHashmi786111726@users.noreply.github.com>
2024-04-23 18:25:45 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,20 +1,20 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-from .models import CarMake, CarModel
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
-import logging
-import json
-from django.views.decorators.csrf import csrf_exempt
-from .populate import initiate
-from .restapis import get_request, analyze_review_sentiments , post_review
+
+from django.http import HttpResponseRedirect, HttpResponse # noqa
+from django.contrib.auth.models import User # noqa
+from django.shortcuts import get_object_or_404, render, redirect # noqa
+from django.contrib.auth import logout # noqa
+from django.contrib import messages # noqa
+from datetime import datetime # noqa
+from .models import CarMake, CarModel # noqa
+from django.http import JsonResponse # noqa
+from django.contrib.auth import login, authenticate # noqa
+import logging # noqa
+import json # noqa
+from django.views.decorators.csrf import csrf_exempt # noqa
+from .populate import initiate # noqa
+from .restapis import get_request, analyze_review_sentiments, post_review # noqa
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -51,7 +51,7 @@ def logout_request(request):
 
 @csrf_exempt
 def registration(request):
-    context = {}
+    context = {} # noqa
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -89,13 +89,13 @@ def registration(request):
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name}) # noqa
+    return JsonResponse({"CarModels": cars})
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 # def get_dealerships(request):
@@ -161,12 +161,12 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
-            return JsonResponse({"status":200})
-        except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+            response = post_review(data) # noqa
+            return JsonResponse({"status": 200})
+        except Exception:
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
